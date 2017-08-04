0337ddba592690df08e4bbaf4098067a67f85ec8
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-08-04 10:13:43 +0530

    initialized Examination Management System Project

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+ /*
+***************************************************
+*** Examination Management System               ***
+*** Faculty of Applied Sciences                 ***
+***---------------------------------------------***
+*** Developer: Lilanka Ravinath                 ***
+***                                             ***
+***************************************************
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <!-- IE Edge Meta Tag -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Optional IE 8 support -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <!--[endif]-->
+        <!-- Minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Optional Theme -->
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <!-- Minified JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- jQuery -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+</html>
