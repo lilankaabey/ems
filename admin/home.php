d20d434e1bbae4ab2bd8490e2446cb9353ef6727
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-08-10 04:17:55 +0530

    modified Account button in nav bar

--- admin/home.php
+++ admin/home.php
@@ -1,8 +1,64 @@
 <?php
+ /*
+***************************************************
+*** Examination Management System               ***
+*** Faculty of Applied Sciences                 ***
+***---------------------------------------------***
+*** Developer: Lilanka Ravinath                 ***
+***                                             ***
+***************************************************
+*/
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <!-- IE Edge Meta Tag -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Optional IE 8 support -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <!--[endif]-->
+        <!-- Minified CSS -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Custome Styles -->
+        <link rel="stylesheet" href="../css/styles.css">
+        <!-- jQuery -->
+        <script src="../js/jquery-3.2.1.min.js"></script>
+        <!-- Minified JavaScript -->
+        <script src="../js/bootstrap.min.js"></script>
+        <!-- Fonts Link -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,100,700,900" rel="stylesheet" type="text/css">
+        <!-- Icon -->
+        <link rel="icon" href="#">
+    </head>
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Examination Management System</a>
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Account
+                        <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> &nbsp;Profile</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-lock"></span> &nbsp;Change Password</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-off"></span> &nbsp;Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- //Navigation -->
+        
+    </body>
+</html>
 
